73513caffbb653960db8422579fb44d0ef2ad947
rohit2005kumar <rohitkumarroy234987@gmail.com>
2025-03-28 10:06:09 +0530

    js file added

---
+++ script.js
@@ -0,0 +1,8 @@
+// let div1=document.querySelector(".inp1");
+// div1.addEventListener("click",function(){
+//     alert("clicked");
+//  let inputele=document.createElement("input");
+//  inputele.value="enter from";
+//  div1.appendChild(inputele);
+
+// })
